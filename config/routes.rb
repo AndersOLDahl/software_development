ddda4319e04531640e6d2743a08dcc9dc7c735ba
Anders Dahl <dahl.a@husky.neu.edu>
2015-11-10 16:34:01 -0500

    Added login feature using the devise gem. Created the user model. Seeded database with test users.. Feel free to add your own email in there. Customized devise to work with a single app application that does not allow for user sign_ups. Added bootstrap and reordered application.js to remove javascript errors. Changed the css files to sass.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
+  devise_for :users, :skip => :registrations
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
