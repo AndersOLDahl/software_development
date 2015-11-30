9c1a53bec3ce50e65a9f67c396b1fcf68259835c
kclaeys <claeys.k@husky.neu.edu>
2015-11-29 20:04:19 -0600

    Adding infowindow support and a test Currently displays site, lat/lon, and location

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,4 +12,14 @@ describe PagesController do
       expect(assigns(:hash)).not_to be_nil
     end
   end
+  describe "myinfowindow" do
+    it "structures an infowindow" do
+      testempty = PagesController.new.myinfowindow("", "", "", "")
+      testempty.length.equal?(197)
+
+      testa12b = PagesController.new.myinfowindow("aaa", "111", "222", "bbb")
+      expect(testa12b).to include "aaa" and include "111" and include "222" and include "bbb"
+      testa12b.length.equal?(testempty.length + "aaabbbcccddd".length)
+    end
+  end
 end
