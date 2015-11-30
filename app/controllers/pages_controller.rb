22406c4bc993b9585f9628705ca94f797fc42e45
Emil Abraham <emilabraham1414@gmail.com>
2015-11-30 18:04:08 -0500

    Got a rough implementation working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   def home
     @microsites = Microsite.all
+    @sites = Microsite.all.collect{|m| m.site}.uniq.sort
+    @country = Microsite.all.collect{|m| m.country}.uniq.sort
+    @state_province = Microsite.all.collect{|m| m.state_province}.uniq.sort
+    @zone = Microsite.all.collect{|m| m.zone}.uniq.sort
+    @sub_zone = Microsite.all.collect{|m| m.sub_zone}.uniq.sort
     @hash = Gmaps4rails.build_markers(@microsites) do |microsite, marker|
       marker.lat microsite.field_lat
       marker.lng microsite.field_lon
