14039aa53e608a72962b07c9a45965fd2b266e05
Anders Dahl <dahl.a@husky.neu.edu>
2015-12-07 00:20:37 -0500

    Removed clutter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,10 +6,15 @@ class PagesController < ApplicationController
       marker.lat microsite.field_lat
       marker.lng microsite.field_lon
       marker.infowindow myinfowindow(microsite.site, microsite.field_lat, microsite.field_lon, microsite.location)
+    end
 
     @search = Search.new(:microsite, params[:search], per_page: 5)
     @search.order = 'microsite_id'
     @search_data = @search.run
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @search_data }
     end
   end
   def myinfowindow(site, lat, lng, location)
